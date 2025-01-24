c29dc49ef932543dca4a7537fbee5da6ea32e655
Sadman Mahmud <you@example.com>
2025-01-24 19:44:25 +0600

    full done

--- project-1/script.js
+++ project-1/script.js
@@ -4,28 +4,20 @@ var menuItems = document.getElementById("menuItems");
 
 menuItems.style.maxHeight = "0px";
 
-function toggleMenu(){
-    if(menuItems.style.maxHeight == "0px"){
-        menuItems.style.maxHeight = "155px"
-    }
-    else{
-        menuItems.style.maxHeight = "0px"
-    }
+function toggleMenu() {
+  if (menuItems.style.maxHeight == "0px") {
+    menuItems.style.maxHeight = "155px";
+  } else {
+    menuItems.style.maxHeight = "0px";
+  }
 }
-
 
 var myname = document.getElementById("myname");
 
-function remove(){
-    if(myname.style.display == "none"){
-        myname.style.display = "flex"
-    }
-    else{
-        myname.style.display = "none"
-    }
+function remove() {
+  if (myname.style.display == "none") {
+    myname.style.display = "flex";
+  } else {
+    myname.style.display = "none";
+  }
 }
-
-
-
-
-
